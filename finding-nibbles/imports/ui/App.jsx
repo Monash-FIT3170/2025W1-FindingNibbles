63268b2ad800f5b5df7569bed55c022f263cc7eb
koharuyanen <knum0001@student.monash.edu>
2025-04-24 11:56:20 +1000

    added some frontend part for start-up and mapscreen (not completed)

--- finding-nibbles/imports/ui/App.jsx
+++ finding-nibbles/imports/ui/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import MapScreen from './MapScreen'; // Make sure this path is correct
 
-export const App = () => (
-  <div>
-    <h1>Congratulations! You are one Step closer to Finding you Nibbles</h1>
-  </div>
-);
+export const App = () => {
+  const login = false; // Change to false to see MapScreen
+
+  return (
+    <div className="app">
+      {login ? (
+        <>
+          <header className="header">
+            <div className="logo">
+              {/* Replace this with an actual icon if you have one */}
+              <img src="/images/bunny-icon.png" alt="bunny icon" className="bunny-icon" />
+              FindingNibbles
+            </div>
+            <button className="account-button">Account</button>
+          </header>
+
+          <div className="main-content">
+            <div className="background-image">
+              <div className="location-box">
+                <h2>What can we eat today...?</h2>
+                <input
+                  type="text"
+                  className="location-input"
+                  placeholder="Enter your location"
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <MapScreen />
+      )}
+    </div>
+  );
+};
