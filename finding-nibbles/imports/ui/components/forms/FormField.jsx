baa1c3a12b5424b23ca60ccfd36a3d420e5f5dc8
Aaron Tran <aarrron.tran@gmail.com>
2025-04-30 01:46:55 +1000

    [Aaron] Rough Draft of Register Form Styling

--- finding-nibbles/imports/ui/components/forms/FormField.jsx
+++ finding-nibbles/imports/ui/components/forms/FormField.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-import "./../../styles/forms.css";
+import "../../styles/forms.css"; 
 
 
-const FormField = ({ label, type, name, placeholder, value, onChange }) => {
-  return (
-    <div className="form-field">
-      <label htmlFor={name} className="form-label">
-        {label}
-      </label>
-      <input
-        type={type}
-        id={name}
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        className="form-input"
-      />
-    </div>
-  );
-};
+export const FormField = ({ label, type, name, placeholder, value, onChange, onClear }) => {
+    return (
+      <div className="form-field">
+        <label htmlFor={name} className="form-label">
+          {label}
+        </label>
+        <div className="input-wrapper">
+          <input
+            type={type}
+            id={name}
+            name={name}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            className="form-input"
+          />
+        </div>
+      </div>
+    );
+  };
 
-export default FormField;
