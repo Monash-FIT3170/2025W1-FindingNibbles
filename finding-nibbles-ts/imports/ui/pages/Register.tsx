cfe495aaa4c9d048a1525af784d432e1daae96ed
Aaron Tran <aarrron.tran@gmail.com>
2025-08-12 23:29:55 +1000

    [Aaron] Error Wording Change

--- finding-nibbles-ts/imports/ui/pages/Register.tsx
+++ finding-nibbles-ts/imports/ui/pages/Register.tsx
@@ -51,7 +51,7 @@ export const Register = () => {
     }, (err) => {
       if (err) {
         console.log(err);
-        toast.error('Registration failed: Duplicate email');
+        toast.error('Duplicate email found in the system');
       } else {
         console.log('User registered and logged in!');
         toast.success('Registered successfully');
