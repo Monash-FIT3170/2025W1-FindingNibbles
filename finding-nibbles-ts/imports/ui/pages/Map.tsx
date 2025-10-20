27e54fa4d283bf42874b90f32523aa8259da740d
Arnav Sethi <aset0018@student.monash.edu>
2025-10-20 20:18:30 +1100

    fixed the api fetching issue

--- finding-nibbles-ts/imports/ui/pages/Map.tsx
+++ finding-nibbles-ts/imports/ui/pages/Map.tsx
@@ -492,7 +492,8 @@ export const Map = () => {
       // console.log('Longitude:', longitude);
       // console.log('Search Radius:', searchRadius);
       
-    const URL = "https://places.googleapis.com/v1/places:searchNearby";
+    const URL =
+      "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${searchRadius}&type=restaurant&key=${API_KEY}";
 
     const requestBody = {
       includedTypes: ["restaurant"],
